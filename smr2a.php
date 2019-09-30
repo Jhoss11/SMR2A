746329280969191ecfc044af2b4fcb92c1c51ab9
jhoss11 <jhostin.alexander98@gmail.com>
2019-09-30 20:00:42 +0200

    timestamp

--- smr2a.php
+++ smr2a.php
@@ -4,8 +4,10 @@ Jhostin Alexander.Smr2a
 
 <?php
 $f=fopen('visitas.txt','a');
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
-fclose($f);
+fwrite($f,date('Y-m-d H:i:s '));
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,"\r\n");
+fclose($f)
 
 ?>
 
