a65eb8285b6b685966a10897ea0ed29af214e7da
jhoss11 <jhostin.alexander98@gmail.com>
2019-10-01 19:30:00 +0200

    formulario

--- smr2a.php
+++ smr2a.php
@@ -7,9 +7,27 @@ $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
-fclose($f)
-
 ?>
 
+<?php
+if(isset($_GET['correo'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['correo']);
+	fwrite($f,"\r\n");
+}
+?>
+
+
+<html>
+<body>
+<h1>FORMULARIO</h1>
+<form>
+	<p>
+		Correo electronico: <input type="email" name="correo">
+	</p>
+	<button>Enviar la informacion</button>
+</form>
+
+</html>
 
 <a href="http://192.168.0.173/smr2a-VCR/smr2a.php">Victor Cabo</a>
