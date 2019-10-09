28b3d0ed37654251ce2a6374dfd3379f4330e2ba
jhoss11 <jhoss11.jhostin.alexander98@gmail.com>
2019-10-09 15:31:12 +0200

    Funciones

---
+++ 2b.php
@@ -0,0 +1,14 @@
+<?php
+function calcula(){
+	$a=4;
+	$b=6;
+	echo ($a+$b).'<br>';
+	echo ($a-$b).'<br>';
+	echo ($a*$b).'<br>';
+	echo ($a/$b).'<br>';
+}
+calcula ();
+calcula ();
+calcula ();
+calcula ();
+?>
