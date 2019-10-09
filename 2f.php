808b048c3b8d21f8d79a6b041a5d92bf1a37c5e2
jhoss11 <jhoss11.jhostin.alexander98@gmail.com>
2019-10-09 16:23:19 +0200

    arrays

---
+++ 2f.php
@@ -0,0 +1,20 @@
+<?php
+$meses=[
+'enero',
+'febrero',
+'marzo',
+'abril',
+'mayo',
+'junio',
+'julio',
+'agosto',
+'septiembre',
+'octubre',
+'noviembre',
+'diciembre',
+];
+for($i=0;$i<12;$i++)
+	echo $meses[$i].'<br>';
+	foreach($meses as $mes)
+		echo $mes.'<br/>';
+?>		
