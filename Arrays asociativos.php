d55f75cd80a2229c1de2fdcf1ba8dfc9a6b017e9
jhoss11 <jhostin.alexander98@gmail.com>
2019-10-22 20:01:20 +0200

    Arrays asociativos

--- Arrays asociativos.php
+++ Arrays asociativos.php
@@ -1,6 +1,46 @@
 
 <?php
 
-$meses =('enero','febrero','marzo','abril','mayo','junio','julio',
-               'agosto','septiembre','octubre','noviembre','diciembre');
-echo $meses;		   
+$meses = [
+'a'=>['mes'=>'Enero','dias'=>'31','estacion'=>'invierno'],
+'b'=>['mes'=>'Febrero','dias'=>'28/29','estacion'=>'invierno'],
+'d'=>['mes'=>'Marzo','dias'=>'31','estacion'=>'primavera'],
+'e'=>['mes'=>'Abril','dias'=>'30','estacion'=>'primavera'],
+'f'=>['mes'=>'Mayo','dias'=>'31','estacion'=>'primavera'],
+'f'=>['mes'=>'Junio','dias'=>'30','estacion'=>'verano'],
+'g'=>['mes'=>'Julio','dias'=>'31','estacion'=>'verano'],
+'h'=>['mes'=>'Agosto','dias'=>'30','estacion'=>'verano'],
+'i'=>['mes'=>'Septiembre','dias'=>'31','estacion'=>'otoño'],
+'j'=>['mes'=>'Octubre','dias'=>'30','estacion'=>'otoño'],
+'k'=>['mes'=>'Noviembre','dias'=>'31','estacion'=>'otoño'],
+'m'=>['mes'=>'Diciembre','dias'=>'30','estacion'=>'invierno'],
+];
+
+
+
+
+
+
+
+
+
+
+/*
+
+echo '<pre>';
+
+print_r($meses);
+echo '<pre>';
+
+echo $meses['b'];
+*/
+foreach($meses as $mes){
+	echo ' Tiene ';
+	echo $mes['dias'].' dias ';
+	echo ' y es ';
+	echo $mes['mes']. ' y su estacion es ';
+	echo $mes['estacion'].'.';
+	echo '<br>';
+}
+?>
+ 
